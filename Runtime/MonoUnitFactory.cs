e2c76f2ca90b353711d9d75f4cf3664cc0786ccc
braty <brataysen@gmail.com>
2024-06-01 20:20:05 +0300

    mono unit factory added

---
+++ Runtime/MonoUnitFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace BratyECS
+{
+    public abstract class MonoUnitFactory<T> : MonoBehaviour where T : MonoUnit<T> 
+    {
+        [SerializeField] private T _prefab;
+        private ObjectPool<T> _pool;
+
+        private ObjectPool<T> Pool {
+            get {
+                if (_pool == null) throw new InvalidOperationException("You need to call InitPool before using it.");
+                return _pool;
+            }
+            set => _pool = value;
+        }
+
+        protected virtual void Awake()
+        {
+            InitPool(_prefab);
+        }
+
+        private void OnEnable()
+        {
+            MonoUnitFactoryManager<T>.Instance = this;
+        }
+
+        private void OnDisable()
+        {
+            MonoUnitFactoryManager<T>.Instance = null;
+        }
+
+        protected void InitPool(T prefab, int initial = 10, int max = 100, bool collectionChecks = false) {
+            _prefab = prefab;
+            Pool = new ObjectPool<T>(
+                CreateSetup,
+                GetSetup,
+                ReleaseSetup,
+                DestroySetup,
+                collectionChecks,
+                initial,
+                max);
+        }
+
+        #region Overrides
+        protected virtual T CreateSetup() => Instantiate(_prefab);
+        protected virtual void GetSetup(T obj) => obj.gameObject.SetActive(true);
+        protected virtual void ReleaseSetup(T obj) => obj.gameObject.SetActive(false);
+        protected virtual void DestroySetup(T obj) => Destroy(obj);
+        #endregion
+
+        #region Getters
+        public T Get() => Pool.Get();
+        public void Release(T obj) => Pool.Release(obj);
+        #endregion
+    }
+}
---
+++ Runtime/MonoUnitFactoryManager.cs
@@ -0,0 +1,17 @@
+﻿namespace BratyECS
+{
+    public static class MonoUnitFactoryManager<T> where T : MonoUnit<T>
+    {
+        internal static MonoUnitFactory<T> Instance;
+
+        public static T Get()
+        {
+            return Instance.Get();
+        }
+
+        public static void Release(T unit)
+        {
+            Instance.Release(unit);
+        }
+    }
+}
