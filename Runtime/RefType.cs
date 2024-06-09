03d812d1fdb38a088394ec0698da6bdfea7870af
boraty14 <bratygame14@gmail.com>
2024-06-09 15:56:58 +0300

    ref type logic added

---
+++ Runtime/RefType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace BratyECS
+{
+    [Serializable]
+    public class RefType<T>
+    {
+        [SerializeField] private T _value;
+        
+        public T Value
+        {
+            get => _value;
+            set => _value = value;
+        }
+        
+        public T ReadValue => _value;
+    }
+}
