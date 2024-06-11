977bd89c95b9bf5cd04f7be492efc2ff8c9791ab
boraty14 <bratygame14@gmail.com>
2024-06-11 04:24:40 +0300

    pooling for monounit

--- Runtime/MonoUnit.cs
+++ Runtime/MonoUnit.cs
@@ -5,5 +5,7 @@ namespace BratyECS
     public abstract class MonoUnit<T> where T : MonoBehaviour
     {
         protected internal T Mono { get; internal set; }
+
+        public abstract void Reset();
     }
 }
--- Runtime/MonoUnitManager.cs
+++ Runtime/MonoUnitManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace BratyECS
 {
@@ -6,31 +7,50 @@ namespace BratyECS
         where TMonoUnit : MonoUnit<TMono>, new() where TMono : MonoBehaviour
     {
         private readonly IMonoFactory<TMono> _monoFactory;
-        
+        private readonly Stack<TMonoUnit> _unitPool;
+
         public MonoUnitManager(IMonoFactory<TMono> monoFactory)
         {
             _monoFactory = monoFactory;
+            _unitPool = new();
         }
 
         public override TMonoUnit AddUnit()
         {
+            TMonoUnit monoUnit = GetUnitFromPool();
+            monoUnit.Reset();
+            
             TMono mono = _monoFactory.CreateMono();
-            TMonoUnit monoUnit = new TMonoUnit
-            {
-                Mono = mono
-            };
+            monoUnit.Mono = mono;
+            
             Units.Add(monoUnit);
             return monoUnit;
         }
 
         public override void RemoveUnit(TMonoUnit unit)
         {
-            Units.Remove(unit);
-            if (unit.Mono == null)
+            if (unit.Mono != null)
             {
-                return;
+                _monoFactory.DeleteMono(unit.Mono);
             }
-            _monoFactory.DeleteMono(unit.Mono);
+
+            Units.Remove(unit);
+            ReturnUnitToPool(unit);
+        }
+
+        private TMonoUnit GetUnitFromPool()
+        {
+            if (_unitPool.Count == 0)
+            {
+                return new TMonoUnit();
+            }
+
+            return _unitPool.Pop();
+        }
+
+        private void ReturnUnitToPool(TMonoUnit unit)
+        {
+            _unitPool.Push(unit);
         }
     }
 }
