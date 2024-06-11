bf0292a2d0364d991a8c8329e24afa4efca7fc8f
boraty14 <bratygame14@gmail.com>
2024-06-11 21:28:44 +0300

    abstract engine added

---
+++ Runtime/Engine.cs
@@ -0,0 +1,17 @@
+﻿namespace BrUnit
+{
+    public abstract class Engine
+    {
+        public void Tick()
+        {
+            if (!IsTickable())
+            {
+                return;
+            }
+            TickEngine();
+        }
+        
+        protected abstract void TickEngine();
+        protected abstract bool IsTickable();
+    }
+}
