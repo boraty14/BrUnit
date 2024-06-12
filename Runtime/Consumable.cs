58b8514fbff8a7898278a60474714f876332f493
boraty14 <bratygame14@gmail.com>
2024-06-12 17:27:31 +0300

    consumable added

---
+++ Runtime/Consumable.cs
@@ -0,0 +1,27 @@
+﻿namespace BrUnit
+{
+    public class Consumable<T>
+    {
+        private bool _isConsumableSet;
+        private T _consumable;
+        
+        public void SetConsumable(T consumable)
+        {
+            _isConsumableSet = true;
+            _consumable = consumable;
+        }
+        
+        public bool TryConsume(out T consumable)
+        {
+            if (!_isConsumableSet)
+            {
+                consumable = default;
+                return false;
+            }
+
+            _isConsumableSet = false;
+            consumable = _consumable;
+            return true;
+        }
+    }
+}
